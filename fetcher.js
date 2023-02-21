1635bc2150f37f55ffd9f2fb1f58d2404c01961d
Sean Gray <seangray.dev@gmail.com>
2023-02-21 00:24:56 +0000

    refactor + check if file exists already

--- fetcher.js
+++ fetcher.js
@@ -1,6 +1,7 @@
 // Require the 'request' and 'fs' modules
 const request = require("request");
 const fs = require("fs");
+const readline = require("readline");
 
 // Get URL and file path arguments from the command line
 const url = process.argv[2];
@@ -12,38 +13,67 @@ if (!url || !filePath) {
   process.exit(1);
 }
 
-// Send GET request to URL using 'request'
-request(url, (error, response, body) => {
-  // print an error message and exit if there was an error
-  if (error) {
-    console.error(`Error: ${error.message}`);
-    return;
-  }
+// check if file path already exists
+fs.access(filePath, fs.constants.F_OK, (err) => {
+  if (!err) {
+    const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout,
+    });
 
-  // print an error message and exit if status code is not 200
-  if (response.statusCode !== 200) {
-    console.error(`Request failed.\nStatus Code: ${response.statusCode}`);
-    return;
+    rl.question(
+      `${filePath} already exists. Do you want to overwrite (Y/N)?`,
+      (answer) => {
+        rl.close();
+        if (answer.toUpperCase() !== "Y") {
+          console.log("Aborting...");
+          process.exit(0);
+        } else {
+          // send GET request to URL
+          downloadResources();
+        }
+      }
+    );
+  } else {
+    // if file doesnt exist, send GET request
+    downloadResources();
   }
+});
 
-  // write the response body to the specified file path with 'fs' if the response is valid.
-  fs.writeFile(filePath, body, (err) => {
-    // print an error message and exit if there is an error writing file
-    if (err) {
-      console.error(`Error writing file: ${err.message}`);
+const downloadResources = function () {
+  // Send GET request to URL using 'request'
+  request(url, (error, response, body) => {
+    // print an error message and exit if there was an error
+    if (error) {
+      console.error(`Error: ${error.message}`);
       return;
     }
 
-    // get file size
-    fs.stat(filePath, (err, stats) => {
+    // print an error message and exit if status code is not 200
+    if (response.statusCode !== 200) {
+      console.error(`Request failed.\nStatus Code: ${response.statusCode}`);
+      return;
+    }
+
+    // write the response body to the specified file path with 'fs' if the response is valid.
+    fs.writeFile(filePath, body, (err) => {
+      // print an error message and exit if there is an error writing file
       if (err) {
-        console.log(`Erro getting file stats: ${err.message}`);
+        console.error(`Error writing file: ${err.message}`);
         return;
       }
 
-      const fileSize = stats.size;
-      // print success message
-      console.log(`Downloaded and saved ${fileSize} bytes to ${filePath}.`);
+      // get file size
+      fs.stat(filePath, (err, stats) => {
+        if (err) {
+          console.log(`Erro getting file stats: ${err.message}`);
+          return;
+        }
+
+        const fileSize = stats.size;
+        // print success message
+        console.log(`Downloaded and saved ${fileSize} bytes to ${filePath}.`);
+      });
     });
   });
-});
+};
